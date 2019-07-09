f238fd804359c69ef0e56cbf80e931ce7cd608c2
Sethu Naga Karthik M <sethu.karthik@hotmail.com>
2019-07-09 19:30:02 +0530

    added percentage calculation method

--- lib/calculation.rb
+++ lib/calculation.rb
@@ -1,17 +1,24 @@
 module Calculation
-
-  def self.price_cal_with_qty(price, req_qty, age, offer_per)
-    cost_per_product = price * req_qty.to_i
+  extend self
+  
+  def price_cal_with_qty(price, req_qty, age, offer_per)
+    cost_per_product = price * req_qty
     if age > 60
-      cost_per_product = cost_per_product.percent_of(offer_per)
+      cost_per_product = offer_per.percent_of(cost_per_product)
     end
+    return cost_per_product
   end
 
-  def self.total_cost(purchased_item)
+  def total_cost(purchased_item)
     total = 0
     purchased_item.map{|item| total += item["price"]}
     return total
   end
+
+end
+
+
+class Numeric
 
   def percent_of(n)
     self.to_f / n.to_f * 100.0
